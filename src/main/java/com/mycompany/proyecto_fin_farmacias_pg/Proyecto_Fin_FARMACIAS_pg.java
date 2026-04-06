4afac4459d6d20023274fc1383a34eb7891215e4
Erick González <erickgoonzalez5@gmail.com>
2026-04-06 12:47:51 -0600

    Creación del proyecto

---
+++ src/main/java/com/mycompany/proyecto_fin_farmacias_pg/Proyecto_Fin_FARMACIAS_pg.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_fin_farmacias_pg;
+
+/**
+ *
+ * @author erick
+ */
+public class Proyecto_Fin_FARMACIAS_pg {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
