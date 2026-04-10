b7dc6f8f3e16f530446bda4b3881bd37c04730b4
Erick González <erickgoonzalez5@gmail.com>
2026-04-10 13:21:13 -0600

    Agregue un metodo en la ventana principal de medicos para rellenar los campos con los datos de la tabla

--- src/main/java/Ventanas/VentanaMedicos.java
+++ src/main/java/Ventanas/VentanaMedicos.java
@@ -34,6 +34,23 @@ public class VentanaMedicos extends javax.swing.JPanel {
         tablaRegMedicos.setModel(modelo);
     });
     timerBusqueda.setRepeats(false);
+    
+    
+    //Selecion de tabla para llenar campos
+    tablaRegMedicos.getSelectionModel().addListSelectionListener(e -> {
+    if (!e.getValueIsAdjusting()) {
+        int fila = tablaRegMedicos.getSelectedRow();
+
+        if (fila != -1) {
+            cajaSSNMedicos.setText(tablaRegMedicos.getValueAt(fila, 0).toString());
+            cajaNombreMedicos.setText(tablaRegMedicos.getValueAt(fila, 1).toString());
+            cajaApPaternoMedicos.setText(tablaRegMedicos.getValueAt(fila, 2).toString());
+            cajaApMaternoMedicos.setText(tablaRegMedicos.getValueAt(fila, 3).toString());
+            cajaEspecialidadMedicos.setText(tablaRegMedicos.getValueAt(fila, 4).toString());
+            cajaExperienciaMedicos.setText(tablaRegMedicos.getValueAt(fila, 5).toString());
+            }
+        }
+    });
     }
 
     /**
@@ -48,15 +65,15 @@ public class VentanaMedicos extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
+        cajaSSNMedicos = new javax.swing.JTextField();
+        cajaNombreMedicos = new javax.swing.JTextField();
+        cajaApPaternoMedicos = new javax.swing.JTextField();
+        cajaApMaternoMedicos = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jTextField5 = new javax.swing.JTextField();
+        cajaEspecialidadMedicos = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
-        jTextField6 = new javax.swing.JTextField();
+        cajaExperienciaMedicos = new javax.swing.JTextField();
         btnAgregarMedicos = new javax.swing.JButton();
         btnEliminarMedicos = new javax.swing.JButton();
         btnEditarMedicos = new javax.swing.JButton();
@@ -79,24 +96,24 @@ public class VentanaMedicos extends javax.swing.JPanel {
         jLabel4.setForeground(new java.awt.Color(241, 245, 249));
         jLabel4.setText("Ap. Paterno:");
 
-        jTextField1.setBackground(new java.awt.Color(71, 85, 105));
+        cajaSSNMedicos.setBackground(new java.awt.Color(71, 85, 105));
 
-        jTextField2.setBackground(new java.awt.Color(71, 85, 105));
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+        cajaNombreMedicos.setBackground(new java.awt.Color(71, 85, 105));
+        cajaNombreMedicos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
+                cajaNombreMedicosActionPerformed(evt);
             }
         });
 
-        jTextField3.setBackground(new java.awt.Color(71, 85, 105));
+        cajaApPaternoMedicos.setBackground(new java.awt.Color(71, 85, 105));
 
-        jTextField4.setBackground(new java.awt.Color(71, 85, 105));
+        cajaApMaternoMedicos.setBackground(new java.awt.Color(71, 85, 105));
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(241, 245, 249));
         jLabel5.setText("Especialidad:");
 
-        jTextField5.setBackground(new java.awt.Color(71, 85, 105));
+        cajaEspecialidadMedicos.setBackground(new java.awt.Color(71, 85, 105));
 
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(241, 245, 249));
@@ -106,7 +123,7 @@ public class VentanaMedicos extends javax.swing.JPanel {
         jLabel7.setForeground(new java.awt.Color(241, 245, 249));
         jLabel7.setText("Años Experiencia:");
 
-        jTextField6.setBackground(new java.awt.Color(71, 85, 105));
+        cajaExperienciaMedicos.setBackground(new java.awt.Color(71, 85, 105));
 
         btnAgregarMedicos.setBackground(new java.awt.Color(40, 40, 40));
         btnAgregarMedicos.setForeground(new java.awt.Color(241, 245, 249));
@@ -164,25 +181,25 @@ public class VentanaMedicos extends javax.swing.JPanel {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(cajaApMaternoMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGap(12, 12, 12)
                                         .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(cajaEspecialidadMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                         .addComponent(jLabel7))
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(cajaSSNMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGap(12, 12, 12)
                                         .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(cajaNombreMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                         .addComponent(jLabel4)))
                                 .addGap(12, 12, 12)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(cajaApPaternoMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(cajaExperienciaMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
                                 .addComponent(jLabel3)
@@ -207,18 +224,18 @@ public class VentanaMedicos extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cajaSSNMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cajaNombreMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cajaApPaternoMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cajaApMaternoMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cajaEspecialidadMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(cajaExperienciaMedicos, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(20, 20, 20)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -234,9 +251,9 @@ public class VentanaMedicos extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
     
     
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+    private void cajaNombreMedicosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cajaNombreMedicosActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
+    }//GEN-LAST:event_cajaNombreMedicosActionPerformed
     
     private void btnAgregarMedicosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarMedicosActionPerformed
         JFrame parent = (JFrame) SwingUtilities.getWindowAncestor(this);
@@ -254,7 +271,13 @@ public class VentanaMedicos extends javax.swing.JPanel {
     private javax.swing.JButton btnAgregarMedicos;
     private javax.swing.JButton btnEditarMedicos;
     private javax.swing.JButton btnEliminarMedicos;
+    private javax.swing.JTextField cajaApMaternoMedicos;
+    private javax.swing.JTextField cajaApPaternoMedicos;
     private javax.swing.JTextField cajaBusqueda;
+    private javax.swing.JTextField cajaEspecialidadMedicos;
+    private javax.swing.JTextField cajaExperienciaMedicos;
+    private javax.swing.JTextField cajaNombreMedicos;
+    private javax.swing.JTextField cajaSSNMedicos;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -263,12 +286,6 @@ public class VentanaMedicos extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
     private javax.swing.JTable tablaRegMedicos;
     // End of variables declaration//GEN-END:variables
 }
